69884f6b7722befedeffda1e6ba569d779a4418d
NDBhalekar <nileshdevidasbhalekar2020@gmail.com>
2025-11-14 07:34:33 +0000

    added 01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 1234
+let accountEmail="nilesh@gmail.com"
+var accountPassword="123423"
+accountCity = "Delhi"
+// accountId=22 // not allowed
+
+console.log(accountId);
+
+accountEmail = "hc@hc.com"
+accountpassword= "21212121"
+accountCity = " Udgir"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+/* we dont use var in js 
+    we can also declare variable without any data type but that not the correct way
+    using console.table we can make table for output
+    we cant change constconsole.table
+*/
